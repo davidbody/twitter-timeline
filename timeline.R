d1bc8b4531b4aa4adc4becf491897f500e6d22a4
David W. Body <davidbody@bigcreek.com>
2017-01-07 14:46:10 -0600

    Base report/plot on tweeters who are 1% or more of timeline

--- timeline.R
+++ timeline.R
@@ -75,11 +75,13 @@ report <- home_timeline_df %>%
     arrange(-total) %>%
     select(screenName, tweets, retweets, replies, total)
 
-# Put data on the top 20 tweeters in long format for plotting
-long_report <- report[1:20,] %>% select(-total) %>% gather(type, count, -screenName)
+# Put data on tweeters constituting 1% or more of timeline in long format for plotting
+one_percent_of_tweets <- as.integer(count(home_timeline_df) / 100)
+long_report <- report %>% filter(total >= one_percent_of_tweets) %>% select(-total) %>% gather(type, count, -screenName)
+long_report$percent <- long_report$count / sum(long_report$count)
 
 # Plot the top tweeters from the timeline
-g <- ggplot(long_report, aes(x = reorder(screenName, count), y = count))
+g <- ggplot(long_report, aes(x = reorder(screenName, percent), y = percent))
 g <- g + geom_bar(stat = "identity", aes(fill = type))
 g <- g + coord_flip()
 g
